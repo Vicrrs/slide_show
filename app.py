b0a9d7ac1f7d0d3ba027f9db1828a62888aea1c2
TK_vicrrs <victor.roza@tarkuss.com.br>
2024-06-11 11:10:03 -0300

    atualiza

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from flask import Flask, jsonify, send_from_directory, render_template
+from flask_socketio import SocketIO
 import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import threading
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 IMAGE_FOLDER = 'images'
 
 @app.route('/images/<filename>')
@@ -18,5 +23,24 @@ def list_images():
 def index():
     return render_template('index.html')
 
+class ImageHandler(FileSystemEventHandler):
+    def on_created(self, event):
+        if event.is_directory:
+            return
+        if event.src_path.lower().endswith(('png', 'jpg', 'jpeg', 'gif')):
+            filename = os.path.basename(event.src_path)
+            socketio.emit('new_image', {'filename': filename})
+
+def start_watching():
+    path = IMAGE_FOLDER
+    event_handler = ImageHandler()
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=False)
+    observer.start()
+    observer.join()
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    watcher_thread = threading.Thread(target=start_watching)
+    watcher_thread.daemon = True
+    watcher_thread.start()
+    socketio.run(app, debug=True)
